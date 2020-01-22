326ccc061bdf1a5b169dcebe66baf8674af5d3fa
x403070567 <x403070567@live.com>
2020-01-22 20:06:56 +0800

    convert electron main.js to main.ts

--- SRC/Script/MainParts/SocketComu.ts
+++ SRC/Script/MainParts/SocketComu.ts
@@ -1,13 +1,8 @@
 import * as express from 'express'
 import * as http from 'http'
 
-class NetServerMain {
+export class NetServerMain {
     constructor(connectPort: number) {
-        express().get('/', function (req, res) {
-            res.send(300);
-        });
         http.createServer(express).listen(connectPort, function () { console.log("connectPort is work at: " + connectPort) });
     }
 }
-
-export = NetServerMain;
---
+++ SRC/Script/MainParts/main.ts
@@ -0,0 +1,19 @@
+import { app, BrowserWindow } from 'electron';
+import { NetServerMain } from './SocketComu'
+
+let Server: NetServerMain;
+
+function createWindow() {
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.loadFile('SRC/HTML/index.html');
+}
+
+app.on('ready', createWindow);
+
+Server = new NetServerMain(5011);
