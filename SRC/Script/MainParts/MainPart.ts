d556a98b7b4004edbb49f49eaa1d738f42938f7b
x403070567 <x403070567@live.com>
2020-04-21 12:27:30 +0800

    react test ready

--- SRC/Script/MainParts/MainPart.ts
+++ SRC/Script/MainParts/MainPart.ts
@@ -1,12 +1,16 @@
 import { EchartShowSys } from './EchartsShow'
 import { NetServerMain } from './SocketComu'
+import { MainPageSet } from './MainUI'
 
 window.onload = function () {
+    MainPageSet();
+}
+
+function ChartsStart() {
     let Gryochart = new EchartShowSys(<HTMLDivElement>document.getElementById('chartGryo'), "Gryo", {
         width: 420,
         height: 300
     }, { ymax: 550, ymin: -550 });
-
     let GryoPitch = Gryochart.EhcartSeriesAdd({
         name: 'Charts',
         type: 'line',
@@ -14,7 +18,6 @@ window.onload = function () {
         hoverAnimation: false,
         data: new Array(30)
     })
-
     let GryoRoll = Gryochart.EhcartSeriesAdd({
         name: 'Charts',
         type: 'line',
@@ -22,7 +25,6 @@ window.onload = function () {
         hoverAnimation: false,
         data: new Array(30)
     })
-
     let GryoYaw = Gryochart.EhcartSeriesAdd({
         name: 'Charts',
         type: 'line',
@@ -35,7 +37,6 @@ window.onload = function () {
         width: 420,
         height: 300
     }, { ymax: 110, ymin: -110 });
-
     let AccelPitch = Accelchart.EhcartSeriesAdd({
         name: 'Charts',
         type: 'line',
@@ -43,7 +44,6 @@ window.onload = function () {
         hoverAnimation: false,
         data: new Array(30)
     })
-
     let AccelRoll = Accelchart.EhcartSeriesAdd({
         name: 'Charts',
         type: 'line',
@@ -51,6 +51,26 @@ window.onload = function () {
         hoverAnimation: false,
         data: new Array(30)
     })
+
+    let FilterChart = new EchartShowSys(<HTMLDivElement>document.getElementById('chartFilter'), "filterCheck", {
+        width: 420,
+        height: 300
+    }, { ymax: 10000, ymin: -10000 })
+    let FilterX_UNFixed = FilterChart.EhcartSeriesAdd({
+        name: 'Charts',
+        type: 'line',
+        showSymbol: false,
+        hoverAnimation: false,
+        data: new Array(100)
+    })
+    let FilterX_Fixed = FilterChart.EhcartSeriesAdd({
+        name: 'ChartsUnfix',
+        type: 'line',
+        showSymbol: false,
+        hoverAnimation: false,
+        data: new Array(100)
+    })
+
 
     let server = new NetServerMain(10086, "192.168.137.1")
 
@@ -60,5 +80,7 @@ window.onload = function () {
         Gryochart.EchartsDataAdd(<number><unknown>server.deviceRTDataBuffer[1][4], GryoYaw);
         Accelchart.EchartsDataAdd(<number><unknown>server.deviceRTDataBuffer[1][5], AccelPitch);
         Accelchart.EchartsDataAdd(<number><unknown>server.deviceRTDataBuffer[1][6], AccelRoll);
+        FilterChart.EchartsDataAdd(<number><unknown>server.deviceRTDataBuffer[1][7], FilterX_UNFixed);
+        FilterChart.EchartsDataAdd(<number><unknown>server.deviceRTDataBuffer[1][8], FilterX_Fixed);
     }, 50)
 }
