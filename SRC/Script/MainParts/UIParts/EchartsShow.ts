b33570c01195515e7974cd8b5aabb5e98dbffce2
x403070567 <x403070567@live.com>
2020-04-21 21:35:22 +0800

    echartshowSys improve

--- SRC/Script/MainParts/UIParts/EchartsShow.ts
+++ SRC/Script/MainParts/UIParts/EchartsShow.ts
@@ -31,13 +31,10 @@ export class EchartShowSys {
     }
 
     constructor(htmlShowArea: HTMLElement, name: string,
-        windowSize: { width: number, height: number },
         chartsSize: { ymax: number, ymin: number }) {
         this._echarts_Area = <HTMLDivElement>htmlShowArea;
         this._echartsMain = echarts.init(this._echarts_Area, {
             renderer: 'canvas',
-            width: windowSize.width,
-            height: windowSize.height
         });
         this._echartsOption.yAxis[0].name = name;
         this._echartsOption.yAxis[0].max = chartsSize.ymax;
@@ -61,90 +58,13 @@ export class EchartShowSys {
         }
     }
 
+    EchartAreaUpdate() {
+        this._echartsMain.resize();
+    }
+
     EhcartSeriesAdd(option: any): number {
         this._ehcartsSeriesCount += 1;
         this._echartsOption.series.push(option);
         return this._ehcartsSeriesCount;
     }
 }
-
-//---================================example===============================================---//
-
-// function ChartsStart() {
-    // let Gryochart = new EchartShowSys(<HTMLDivElement>document.getElementById('chartGryo'), "Gryo", {
-    //     width: 420,
-    //     height: 300
-    // }, { ymax: 550, ymin: -550 });
-    // let GryoPitch = Gryochart.EhcartSeriesAdd({
-    //     name: 'Charts',
-    //     type: 'line',
-    //     showSymbol: false,
-    //     hoverAnimation: false,
-    //     data: new Array(30)
-    // })
-    // let GryoRoll = Gryochart.EhcartSeriesAdd({
-    //     name: 'Charts',
-    //     type: 'line',
-    //     showSymbol: false,
-    //     hoverAnimation: false,
-    //     data: new Array(30)
-    // })
-    // let GryoYaw = Gryochart.EhcartSeriesAdd({
-    //     name: 'Charts',
-    //     type: 'line',
-    //     showSymbol: false,
-    //     hoverAnimation: false,
-    //     data: new Array(30)
-    // })
-
-    // let Accelchart = new EchartShowSys(<HTMLDivElement>document.getElementById('chartAccel'), "Accel", {
-    //     width: 420,
-    //     height: 300
-    // }, { ymax: 110, ymin: -110 });
-    // let AccelPitch = Accelchart.EhcartSeriesAdd({
-    //     name: 'Charts',
-    //     type: 'line',
-    //     showSymbol: false,
-    //     hoverAnimation: false,
-    //     data: new Array(30)
-    // })
-    // let AccelRoll = Accelchart.EhcartSeriesAdd({
-    //     name: 'Charts',
-    //     type: 'line',
-    //     showSymbol: false,
-    //     hoverAnimation: false,
-    //     data: new Array(30)
-    // })
-
-    // let FilterChart = new EchartShowSys(<HTMLDivElement>document.getElementById('chartFilter'), "filterCheck", {
-    //     width: 420,
-    //     height: 300
-    // }, { ymax: 10000, ymin: -10000 })
-    // let FilterX_UNFixed = FilterChart.EhcartSeriesAdd({
-    //     name: 'Charts',
-    //     type: 'line',
-    //     showSymbol: false,
-    //     hoverAnimation: false,
-    //     data: new Array(100)
-    // })
-    // let FilterX_Fixed = FilterChart.EhcartSeriesAdd({
-    //     name: 'ChartsUnfix',
-    //     type: 'line',
-    //     showSymbol: false,
-    //     hoverAnimation: false,
-    //     data: new Array(100)
-    // })
-
-
-    // let server = new NetServerMain(10086, "192.168.137.1")
-
-    // setInterval(function () {
-    //     Gryochart.EchartsDataAdd(<number><unknown>server.deviceRTDataBuffer[1][2], GryoPitch);
-    //     Gryochart.EchartsDataAdd(<number><unknown>server.deviceRTDataBuffer[1][3], GryoRoll);
-    //     Gryochart.EchartsDataAdd(<number><unknown>server.deviceRTDataBuffer[1][4], GryoYaw);
-    //     Accelchart.EchartsDataAdd(<number><unknown>server.deviceRTDataBuffer[1][5], AccelPitch);
-    //     Accelchart.EchartsDataAdd(<number><unknown>server.deviceRTDataBuffer[1][6], AccelRoll);
-    //     FilterChart.EchartsDataAdd(<number><unknown>server.deviceRTDataBuffer[1][7], FilterX_UNFixed);
-    //     FilterChart.EchartsDataAdd(<number><unknown>server.deviceRTDataBuffer[1][8], FilterX_Fixed);
-    // }, 50)
-// }
--- SRC/Script/MainParts/UIParts/MainUI.tsx
+++ SRC/Script/MainParts/UIParts/MainUI.tsx
@@ -3,137 +3,103 @@ import * as ReactDOM from 'react-dom';
 import { EchartShowSys } from './EchartsShow'
 import { NetServerMain } from '../SocketComu'
 
-export function MainPageSet(): void {
-    ReactDOM.render(
-        (
-            <div>
-                <h1>hello</h1>
-                <MainPageUI />
-            </div>
-        ),
-        document.getElementById('root')
-    )
-}
 
-class MainPageUI extends React.Component {
-    MapArea: JSX.Element;
-    SensorRTChart: JSX.Element;
-    SensorRTDashBorad: JSX.Element;
+export module MainPageUI {
+    let server: NetServerMain;
 
-    public render(): JSX.Element {
-        return (
-            <div id="MainPage">
-                <div id="RTChart"><SensorRTChart /></div>
-                <div id="SensorRTChart"></div>
-                <div id="SensorRTDashBorad"></div>
-            </div>
-        );
-    }
-}
-
-class SensorRTChart extends React.Component {
-    private TimerID: NodeJS.Timeout;
-    private Gryochart: EchartShowSys;
-    private server: NetServerMain;
-    private GryoPitch;
-    private GryoRoll;
-    private GryoYaw;
-
-    public render(): JSX.Element {
-        const css = "#chartGryo{ width: 600px;height: 400px;}";
-        return (
-            <div>
-                <style>
-                    {css}
-                </style>
-                <div id='chartGryo'></div>
-            </div >
+    export function MainPageSet(): void {
+        server = new NetServerMain(10086, "192.168.137.1")
+        ReactDOM.render(
+            (
+                < MainPageUI />
+            ),
+            document.getElementById("root")
         )
     }
 
-    componentDidMount() {
-        this.SensorRTChartInit();
-        this.server = new NetServerMain(10086, "192.168.137.1")
-        setInterval(() => {
-            this.Gryochart.EchartsDataAdd(Number(this.server.deviceRTDataBuffer[1][2]), this.GryoPitch);
-            this.Gryochart.EchartsDataAdd(Number(this.server.deviceRTDataBuffer[1][3]), this.GryoRoll);
-            this.Gryochart.EchartsDataAdd(Number(this.server.deviceRTDataBuffer[1][4]), this.GryoYaw);
-        }, 50);
+    class MainPageUI extends React.Component {
+        MapArea: JSX.Element;
+        SensorRTChart: JSX.Element;
+        SensorRTDashBorad: JSX.Element;
+
+        public render(): JSX.Element {
+            return (
+                <div id="MainPage">
+                    <div id="Map"></div>
+                    <div id="SensorRTChart"><SensorRTChart /></div>
+                    <div id="SensorRTDashBorad"></div>
+                    <div id="GLRTShow"></div>
+                </div>
+            );
+        }
     }
 
-    componentWillUnmount() {
-        clearInterval(this.TimerID);
+    class Map extends React.Component {
+
     }
 
-    private SensorRTChartInit() {
-        this.Gryochart = new EchartShowSys(document.getElementById('chartGryo'), "Gryo", {
-            width: 600,
-            height: 400
-        }, { ymax: 550, ymin: -550 });
-        this.GryoPitch = this.Gryochart.EhcartSeriesAdd({
-            name: 'Charts',
-            type: 'line',
-            showSymbol: false,
-            hoverAnimation: false,
-            data: new Array(30)
-        })
-        this.GryoRoll = this.Gryochart.EhcartSeriesAdd({
-            name: 'Charts',
-            type: 'line',
-            showSymbol: false,
-            hoverAnimation: false,
-            data: new Array(30)
-        })
-        this.GryoYaw = this.Gryochart.EhcartSeriesAdd({
-            name: 'Charts',
-            type: 'line',
-            showSymbol: false,
-            hoverAnimation: false,
-            data: new Array(30)
-        })
+    class SensorRTChart extends React.Component {
+        private TimerID: NodeJS.Timeout;
+        private Gryochart: EchartShowSys;
+        private GryoPitch: number;
+        private GryoRoll: number;
+        private GryoYaw: number;
+
+        public render(): JSX.Element {
+            const css = "#chartGryo{ width: 600px;height: 400px;}";
+            return (
+                <div>
+                    <style>{css}</style>
+                    <div id='chartGryo'></div>
+                </div >
+            )
+        }
+
+        componentDidMount() {
+            this.SensorRTChartInit();
+            setInterval(() => {
+                this.Gryochart.EchartsDataAdd(Number(server.deviceRTDataBuffer[1][2]), this.GryoPitch);
+                this.Gryochart.EchartsDataAdd(Number(server.deviceRTDataBuffer[1][3]), this.GryoRoll);
+                this.Gryochart.EchartsDataAdd(Number(server.deviceRTDataBuffer[1][4]), this.GryoYaw);
+                this.Gryochart.EchartAreaUpdate();
+            }, 50);
+        }
+
+        componentWillUnmount() {
+            clearInterval(this.TimerID);
+        }
+
+        private SensorRTChartInit() {
+            this.Gryochart = new EchartShowSys(document.getElementById('chartGryo'), "Gryo", { ymax: 550, ymin: -550 });
+            this.GryoPitch = this.Gryochart.EhcartSeriesAdd({
+                name: 'Charts',
+                type: 'line',
+                showSymbol: false,
+                hoverAnimation: false,
+                data: new Array(30)
+            })
+            this.GryoRoll = this.Gryochart.EhcartSeriesAdd({
+                name: 'Charts',
+                type: 'line',
+                showSymbol: false,
+                hoverAnimation: false,
+                data: new Array(30)
+            })
+            this.GryoYaw = this.Gryochart.EhcartSeriesAdd({
+                name: 'Charts',
+                type: 'line',
+                showSymbol: false,
+                hoverAnimation: false,
+                data: new Array(30)
+            })
+        }
+    }
+
+    class SensorRTDashBorad extends React.Component {
+
+    }
+
+    class GLRTShow extends React.Component {
+
     }
 }
-
-// interface Person {
-//     name: string
-// }
-
-// interface States {
-//     date: Date
-// }
-
-// class MainPageUI extends React.Component<Person, States> {
-//     timerID: NodeJS.Timeout;
-
-//     constructor(props) {
-//         super(props);
-//         this.state = { date: new Date() };
-//     }
-
-//     componentDidMount(): void {
-//         this.timerID = setInterval(
-//             () => this.tick(),
-//             1000
-//         );
-//     }
-
-//     componentWillUnmount(): void {
-//         clearInterval(this.timerID);
-//     }
-
-//     tick() {
-//         this.setState({
-//             date: new Date()
-//         });
-//     }
-
-//     render(): JSX.Element {
-//         return (
-//             <div>
-//                 <h1>Hello, world!</h1>
-//                 <h2>It is {this.props.name}</h2>
-//                 <h3>{this.state.date.toLocaleTimeString()}.</h3>
-//             </div>
-//         );
-//     }
-// }
